573fe24e4ac704301a37bdcb8274fc4150168815
Juanky <juancamiloherrera960@gmail.com>
2024-05-05 01:18:37 -0500

    feat: Crear nota, Bg, Btn Save, Guardian
    Se agrega funcionalidad para crear la nota, background, nuevo botón de guardar y guardian para que no puedas entrar a crear una nota sin ingresar el usuario

--- assets/js/newnote.js
+++ assets/js/newnote.js
@@ -1,3 +1,10 @@
+const notesUrl = "http://localhost:5202/api/notes";
+
+const user = localStorage.getItem("userID");
+if (!user) {
+    location.href = "../../index.html";
+}
+
 const inputTile = document.querySelector(".inputTitle");
 const inputP = document.querySelector(".inputP");
 
@@ -10,3 +17,28 @@ inputP.addEventListener("input", () => {
     inputP.style.height = "auto";
     inputP.style.height = inputP.scrollHeight + "px";
 })
+
+const saveNote = document.querySelector("#saveNote");
+
+saveNote.addEventListener("click", () => {
+    let date = new Date();
+    date = new Date(date.getTime() - (date.getTimezoneOffset() * 60000));
+    date = date.toISOString();
+
+    datos = {
+        title: inputTile.value,
+        content: inputP.value,
+        userID: user,
+        dateModified: date
+    }
+
+    fetch(notesUrl, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(datos)
+    }).then(r => r.json()).then((newNote) => {
+        console.log(newNote.dateModified);
+        alert(`La nota "${newNote.title}" se creo correctamente`)
+        location.href = "../../index.html";
+    });
+});
